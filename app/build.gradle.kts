46e42de7caf37a42bd2fbd09d3c3b95b1336c188
pessima_au <144061972+pessima-au@users.noreply.github.com>
2026-07-12 07:47:08 +0000

    build: update application ID to lyricsflow.verse

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "au.com.pessima.verse"
+    applicationId = "au.com.lyricsflow.verse"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
